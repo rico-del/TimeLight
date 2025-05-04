3958cc004160b03599090834c321b52bc83e4e9a
Derick Omondi <derickomondi591@gmail.com>
2025-05-04 16:08:54 +0300

    Delete TimeLight directory

--- TimeLight/js/notifications.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menuToggle');
-const sidebar = document.getElementById('sidebar');
-
-menuToggle.addEventListener('click', () => {
-  sidebar.classList.toggle('hidden');
-});
--- TimeLight/js/welcome.js
+++
@@ -1,9 +0,0 @@
-const moons=document.querySelectorAll('.moon')
-moons.for each(moon=>{
-    moon.addEventListener('mouseover', function(){
-        moon.style.transform='scale(1.2)';
-    })
-    moon.addEventListener('mouseover', function(){
-        moon.style.transform='scale(1)';
-    })
-})
